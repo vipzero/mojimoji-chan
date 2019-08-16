c5cc82b903eaf0ae165f48ca4e8c292c826f00e1
vipzero <hiroflighter@gmail.com>
2019-08-16 13:32:59 +0900

    add material ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import useLocalStorage from 'react-use/lib/useLocalStorage'
 import chch from 'chch'
 import { Post } from 'chch/dist/dump'
+import { Button } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import { TextField } from '@material-ui/core'
+import { AppBar } from '@material-ui/core'
 
 function App() {
 	const [watchId, setWatchId] = React.useState<NodeJS.Timeout | null>(null)
@@ -10,9 +14,17 @@ function App() {
 	const [url, setUrl] = useLocalStorage<string>('url', '')
 	return (
 		<div>
-			<h1>読み上げちゃん</h1>
+			<TextField
+				label="URL"
+				value={url}
+				fullWidth
+				onChange={v => setUrl(v.target.value)}
+			/>
 			{!isWatch && (
-				<button
+				<Button
+					variant="outlined"
+					color="default"
+					size="large"
 					onClick={async () => {
 						setPosts([])
 						const id = await chch.watch(url, true, post => {
@@ -23,19 +35,26 @@ function App() {
 					}}
 				>
 					読み上げ開始
-				</button>
+				</Button>
 			)}
 			{isWatch && (
-				<button
+				<Button
+					variant="outlined"
+					color="primary"
+					size="large"
 					onClick={() => {
 						clearInterval(watchId)
 						setWatchId(null)
 					}}
 				>
 					終了
-				</button>
+				</Button>
 			)}
-			<input value={url} onChange={v => setUrl(v.target.value)} />
+			<br />
+			<Typography variant="caption">
+				(対応確認済み: VIP,パー速VIP) (収集時間: 1分おき) (読み上げ省略:
+				40文字以降)
+			</Typography>
 		</div>
 	)
 }
